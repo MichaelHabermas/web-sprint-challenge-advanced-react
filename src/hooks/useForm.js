256cd4aae6adf16ae69534399444126154f59235
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-07 08:12:23 -0400

    commented code

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,13 +2,17 @@
 import { useState } from 'react';
 
 export const useForm = initialValues => {
+	// control the state of the success message
 	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+	// control the form input values
 	const [values, setValues] = useState(initialValues);
 
+	// update form values for each field as characters are inputted in their respective fields
 	const handleChanges = e => {
 		setValues({ ...values, [e.target.name]: e.target.value });
 	};
 
+	// on clicking the submit button, display the info with a success message
 	const handleSubmit = e => {
 		e.preventDefault();
 		setShowSuccessMessage(true);
