07b688760b75dbec2bb661c731f0c4245b0eec94
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-07 07:17:35 -0400

    custom hook built and functional

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { queryByText, render, screen, waitFor } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import CheckoutForm from './CheckoutForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test('form header renders', () => {
 	render(<CheckoutForm />);
+	const header = screen.queryByText(/react plants/i);
+	expect(header).toBeTruthy();
+	expect(header).toBeInTheDocument();
+	expect(header).toHaveTextContent(/react plants/i);
 });
 
-test('form shows success message on submit with form details', () => {
+test('form shows success message on submit with form details', async () => {
 	render(<CheckoutForm />);
 });
